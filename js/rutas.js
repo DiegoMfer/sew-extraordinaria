71b467049ce8bf19366074e73a5724344c355b9f
Diego Martín Fernández <diegomartinfnz@gmail.com>
2023-03-20 20:02:39 +0100

    a

--- js/rutas.js
+++ js/rutas.js
@@ -22,34 +22,34 @@ class Rutas {
 
                     //------------------- nombre_ruta ---------------------
                     let nombre_ruta = $(this).find('nombre_ruta').text();
-                    $("main:last-child").append("<p> Nombre ruta: " + nombre_ruta + "</p>");
+                    $("main li:last").append("<p> Nombre ruta: " + nombre_ruta + "</p>");
                     //------------------- tipo_ruta ----------------------------
                     let tipo_ruta = $(this).find('tipo_ruta').text();
-                    $("main:last-child").append("<p> Tipo ruta: " + tipo_ruta + "</p>");
+                    $("main li:last").append("<p> Tipo ruta: " + tipo_ruta + "</p>");
                     //------------------- transporte ----------------------------
                     let transporte = $(this).find('transporte').text();
-                    $("main:last-child").append("<p> Transporte: " + transporte + "</p>");
+                    $("main li:last").append("<p> Transporte: " + transporte + "</p>");
                     //------------------- fecha_inicio ----------------------------
                     let fecha_inicio = $(this).find('fecha_inicio').text();
-                    $("main:last-child").append("<p> Fecha inicio: " + fecha_inicio + "</p>");
+                    $("main li:last").append("<p> Fecha inicio: " + fecha_inicio + "</p>");
                     //------------------- hora_inicio ----------------------------
                     let hora_inicio = $(this).find('hora_inicio').text();
-                    $("main:last-child").append("<p> Hora inicio: " + hora_inicio + "</p>");
+                    $("main li:last").append("<p> Hora inicio: " + hora_inicio + "</p>");
                     //------------------- duracion ----------------------------
                     let duracion = $(this).find('duracion').text();
-                    $("main:last-child").append("<p> Duracion: " + duracion + "</p>");
+                    $("main li:last").append("<p> Duracion: " + duracion + "</p>");
                     //------------------- agencia ----------------------------
                     let agencia = $(this).find('agencia').text();
-                    $("main:last-child").append("<p> Agencia: " + agencia + "</p>");
+                    $("main li:last").append("<p> Agencia: " + agencia + "</p>");
                     //------------------- descripcion ----------------------------
                     let descripcion = $(this).find('descripcion').text();
-                    $("main:last-child").append("<p> Agencia: " + descripcion + "</p>");
+                    $("main li:last").append("<p> Agencia: " + descripcion + "</p>");
                     //-------------------- personas_adecuadas --------------------------
                     let personas_adecuadas = $(this).find('personas_adecuadas').text();
-                    $("main:last-child").append("<p> Personas adecuadas: " + personas_adecuadas + "</p>");
+                    $("main li:last").append("<p> Personas adecuadas: " + personas_adecuadas + "</p>");
                     //------------------- lugar_inicio ---------------------------
                     let lugar_inicio = $(this).find('lugar_inicio').text();
-                    $("main:last-child").append("<p> Lugar inicio: " + lugar_inicio + "</p>");
+                    $("main li:last").append("<p> Lugar inicio: " + lugar_inicio + "</p>");
                     //------------------- direccion_inicio ---------------------------
                     let direccion_inicio = $(this).find('direccion_inicio').text();
                     //-------------------- longitud --------------------------
